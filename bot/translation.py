6ce62fcb98f092689989122f89837ecf2b07d589
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-07-15 10:51:18 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,19 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b>mowna{}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
+only use @bhddhhddnjd group 👊
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+  
+
+  
+
+
+
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@bhddhhddnjd
 </i>"""    
     
     HELP_TEXT = """
@@ -43,12 +49,12 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@bhddhhddnjd
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> on air movies  Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">on air movies </a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
